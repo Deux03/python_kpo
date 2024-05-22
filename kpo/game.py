8617fc52418fb18d1f252ccf1c8fd3b2e826c53f
Nagy Lóránt <h267962@stud.u-szeged.hu>
2024-05-22 15:50:53 +0200

    res free settings and fruit spawn adjustment/speed

--- kpo/game.py
+++ kpo/game.py
@@ -8,7 +8,8 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Fruit Nija")
-        self.screen = pygame.display.set_mode((1400, 800))
+        self.current_resolution = (1400, 800)
+        self.screen = pygame.display.set_mode(self.current_resolution)
         self.clock = pygame.time.Clock()
         self.fruits = []
         self.fruit_types = ['watermelon', 'apple', 'banana']
@@ -22,10 +23,14 @@ class Game:
         self.game_started = False
         self.start_ticks = None
         self.buttons_rects = {
-            'restart_button_rect': pygame.Rect(600, 500, 200, 50),
-            'start_button_rect': pygame.Rect(600, 420, 200, 50),
-            'settings_button_rect': pygame.Rect(600, 500, 200, 50),
-            'quit_button_rect': pygame.Rect(600, 600, 200, 50)
+            'restart_button_rect': pygame.Rect(self.current_resolution[0] // 2 - 100,
+                                               self.current_resolution[1] // 2 + 100, 200, 50),
+            'start_button_rect': pygame.Rect(self.current_resolution[0] // 2 - 100, self.current_resolution[1] // 2,
+                                             200, 50),
+            'settings_button_rect': pygame.Rect(self.current_resolution[0] // 2 - 100,
+                                                self.current_resolution[1] // 2 + 100, 200, 50),
+            'quit_button_rect': pygame.Rect(self.current_resolution[0] // 2 - 100,
+                                            self.current_resolution[1] // 2 + 200, 200, 50)
         }
         try:
             self.font = pygame.font.Font('fonts/comic.ttf', 30)
@@ -82,11 +87,11 @@ class Game:
 
     def display_game_over(self):
         u_lost_text = self.font.render(f'YOU LOST!', True, (255, 0, 0))
-        self.screen.blit(u_lost_text, (600, 300))
+        self.screen.blit(u_lost_text, (self.current_resolution[0] // 2 - 100, self.current_resolution[1] // 2 - 100))
         total_score = self.font.render(f'Total score: {self.score}', True, (255, 0, 0))
-        self.screen.blit(total_score, (600, 340))
+        self.screen.blit(total_score, (self.current_resolution[0] // 2 - 100, self.current_resolution[1] // 2 - 50))
         timer_text = self.font.render(f'Time: {self.end_time:.2f}s', True, (255, 0, 0))
-        self.screen.blit(timer_text, (600, 380))
+        self.screen.blit(timer_text, (self.current_resolution[0] // 2 - 100, self.current_resolution[1] // 2))
 
     def display_button(self, mouse_x, mouse_y, btn_rect, text):
         hover = btn_rect.collidepoint(mouse_x, mouse_y)
@@ -98,7 +103,7 @@ class Game:
 
     def speed_increaser(self, current_time):
         if current_time - self.last_speed_increase_time > self.speed_increase_interval:
-            self.fruit_speed -= 1.2
+            self.fruit_speed -= 1.05
             self.last_speed_increase_time = current_time
 
     def fruits_movement(self, mouse_x, mouse_y):
@@ -119,7 +124,7 @@ class Game:
                 self.lives -= 1
 
     def spawn_random_fruits(self):
-        if random.randint(0, 30) == 0:
+        if random.randint(0, 40) == 0:
             fruit_type = random.choice(self.fruit_types)
             self.fruits.append(Fruit(fruit_type, self.fruit_speed))
 
